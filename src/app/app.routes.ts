6dfc559b8f9c255b78e633f8e778067bcc91e876
ArseniiShub <arsenii.shub@sharp-dev.net>
2024-02-21 18:02:36 +0100

    Initial Routing & Styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from "./login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }
+];
